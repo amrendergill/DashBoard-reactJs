27c7c5159dc641965894090e54142335b57f0106
amrendergill <amrender028@gmail.com>
2022-06-19 23:12:35 +0530

    readne.md

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  };
+© 2022 GitHub, Inc.
+Terms
+Privacy
